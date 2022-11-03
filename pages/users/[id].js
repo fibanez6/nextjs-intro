ad38146eba7146a8de28849575896b49afa0e2fe
Fernando Ibanez <fernando.ibanez@bjss.com>
2022-11-04 10:53:15 +1100

    comments updated

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,5 +1,7 @@
 // This will generate an user details page for each user in the fetched array
-// It will only run during build in production, it will not be called during runtime. You can validate code written inside getStaticPaths is removed from the client-side bundle
+// It will only run during build in production, it will not be called during
+// runtime. You can validate code written inside getStaticPaths is removed from
+// the client-side bundle
 // Generates `/users/1`, `/users/2` and so on.
 export const getStaticPaths = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -14,6 +16,15 @@ export const getStaticPaths = async () => {
 
   return {
     paths, // [{}, {}, {params: {id: 1}}]
+    // FALSE 
+    // -- paths contains all supported meetupId values, otherwise 404 error
+    // -- getStaticProps runs during next build for any paths returned during build
+    // TRUE 
+    // -- it would try to generate a page for this meetupId dynamically on the
+    // server for the incoming request.
+    // -- getStaticProps runs in the background 
+    // BLOCKING 
+    // -- getStaticProps is called before initial render 
     fallback: false, // can also be true or 'blocking'
   }
 }
