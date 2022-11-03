34961895e040cf76d8c222b876f8a5927f785425
Fernando Ibanez <fernando.ibanez@bjss.com>
2022-11-04 10:41:08 +1100

    comments and readme updates

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,4 +1,6 @@
 // This will generate an user details page for each user in the fetched array
+// It will only run during build in production, it will not be called during runtime. You can validate code written inside getStaticPaths is removed from the client-side bundle
+// Generates `/users/1`, `/users/2` and so on.
 export const getStaticPaths = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await res.json();
@@ -12,11 +14,12 @@ export const getStaticPaths = async () => {
 
   return {
     paths, // [{}, {}, {params: {id: 1}}]
-    fallback: false
+    fallback: false, // can also be true or 'blocking'
   }
 }
 
-// it will runs this function #users times 
+// `getStaticPaths` requires using `getStaticProps`
+// It will runs this function #users times by getStaticPaths
 export const getStaticProps = async (context) => {
   const id = context.params.id;
   const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
